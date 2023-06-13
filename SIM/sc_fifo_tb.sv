8ac7be90194b22620251c6fdb8cc5ae7cdb4690b
RB <belenkov_rn@kursksu.ru>
2023-06-13 21:47:02 +0300

    Rev1.3
    Add coverage for "wr" and "rd" signals

--- SIM/sc_fifo_tb.sv
+++ SIM/sc_fifo_tb.sv
@@ -85,7 +85,7 @@ module sc_fifo_tb();
                 #2;
                 wr = 1'b0;
             end
-            repeat($urandom_range(2,1)) @(posedge clk);
+            repeat($urandom_range(2,0)) @(posedge clk);
         end
     end
 
@@ -102,13 +102,50 @@ module sc_fifo_tb();
                 end 
                 else begin
                     $display("Transaction FAILED, data = %0h", data_out);
+                    $display("***TEST FAILED***", data_out);
                     $stop();
                 end
                 rd = 1'b0;
-                repeat($urandom_range(2,5)) @ (posedge clk);
+                repeat($urandom_range(0,5)) @ (posedge clk);
             end
-            @(posedge clk);
+            else begin
+                @(posedge clk);
+            end
         end
     end
+
+
+//  Covergroup: cg_write
+//
+covergroup cg_wr_fifo @(posedge clk);
+    //  Coverpoint: c1
+    c1: coverpoint wr {
+        bins b1 = (0=>0=>0);
+        bins b2 = (0=>0=>1);
+        bins b3 = (0=>1=>0);
+        bins b4 = (0=>1=>1);
+        bins b5 = (1=>0=>0);
+        bins b6 = (1=>0=>1);
+        bins b7 = (1=>1=>0);
+        bins b8 = (1=>1=>1);
+    }
+endgroup
+
+
+covergroup cg_rd_fifo @ (posedge clk);
+    c1: coverpoint rd {
+        bins b1 = (0=>0=>0);
+        bins b2 = (0=>0=>1);
+        bins b3 = (0=>1=>0);
+        bins b4 = (0=>1=>1);
+        bins b5 = (1=>0=>0);
+        bins b6 = (1=>0=>1);
+        bins b7 = (1=>1=>0);
+        bins b8 = (1=>1=>1);
+    }
+endgroup
+
+cg_wr_fifo cg_wr_fifo_inst = new();
+cg_rd_fifo cg_rd_fifo_inst = new();
 
 endmodule
